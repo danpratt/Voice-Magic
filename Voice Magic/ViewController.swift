f1679c4cf719c10ccfb1ce8dfef6d27600f4468e
Daniel Pratt <Daniel@daniels-imac.local>
2017-01-18 12:24:17 -0800

    Initial Commit

---
+++ Voice Magic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Voice Magic
+//
+//  Created by Daniel Pratt on 1/18/17.
+//  Copyright © 2017 Daniel Pratt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
